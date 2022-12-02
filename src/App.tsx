830a5a728c35e11f70d11cd54143aa832fc0c9ee
Carlos Guevara <carlosguevara135@gmail.com>
2022-12-01 20:05:54 -0400

    (add) select to order amount of cads per row

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,53 @@ import { useState, useEffect } from 'react'
 import {getProductsList} from './helpers/getProducts'
 import { IProductsList } from './types/interfaces'
 import Product from './components/Product'
+import Grid from '@mui/material/Grid';
+import { Select } from "@mui/material";
+import MenuItem from '@mui/material/MenuItem';
 import './App.css'
 
 
 
 function App() {
   const [productList, setProductList] = useState<IProductsList>()
+  const [col,setCol]=useState(4)
+    
     useEffect(() => {
-      const handlePokemons = async () => {
+      const handleproducts = async () => {
         const products = await getProductsList();
         setProductList(products);
       };
-      handlePokemons();
+      handleproducts();
     }, []);
+    
+    
+    const styleList = {
+      display:'grid',
+      gridTemplateColumns: `repeat(${col}, 1fr)`,
+      gap: 10,
+      justifyItems: 'center'
+    }
+
   return (
     <div className="App">
-      {productList?.products.map((product) => {
-            return <Product key={product.id} products={product}/>
-          })}
+          <Select color="primary"
+            labelId="Selecciona cantidad de columnas"
+            id="select"
+            value={col}
+            label="Age"
+            onChange={e => setCol(Number(e.target.value))}
+          >
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          </Select >
+      <div className="productlist" style={styleList}>
+        {productList?.products.map((product) => {
+              return <Product key={product.id} products={product}/>
+            })}
+      </div>
+        
     </div>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,23 +13,22 @@ export const Product = ({products}: ProductProps) => {
     const { title, description,thumbnail } = products;
   return (
     <>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={thumbnail}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        {description}
-        </Typography>
-      </CardContent>
-    </Card>
-    <br />
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={thumbnail}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {description}
+          </Typography>
+        </CardContent>
+      </Card>
     </>
   )
 }
